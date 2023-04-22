d95854fc7e8d472a3af374a8039f41705e94879b
Conner <110003454+ConnerConnerConner@users.noreply.github.com>
2023-04-22 18:09:14 -0500

    Create downgrade-helper.bat

---
+++ downgrade-helper.bat
@@ -0,0 +1,36 @@
+@echo off
+
+:: Winget Reminder
+echo You will need to have Winget installed to use this
+pause
+cls
+
+:: Downloading neccesary files to run script
+powershell -Command "Invoke-WebRequest https://ipfs.io/ipfs/QmUgMEVK2nAAMPyEKxN3CVLiSoNaTJem7Vn1G12QxgYV3o -OutFile 18-2-0.dfu"
+powershell -Command "Invoke-WebRequest https://cdn.numworks.com/81cc6426/numworks-driver-win64.msi -OutFile numworks-driver-win64.msi"
+powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1071403053179219998/1071413767872729128/unpack_windows.py -OutFile unpack_windows.py"
+
+:: Installing Numworks Driver
+start .\numworks-driver-win64.msi
+pause
+cls
+
+:: Installing Python using Winget
+winget install -e --id Python.Python.3.9
+
+:: Run unpack_windows.py to unpack 18-2-0.dfu into external.bin and internal.bin
+python .\unpack_windows.py
+
+:: Deleting Unneccsary Files
+del .\numworks-driver-win64.msi
+del .\unpack_windows.py
+del .\18-2-0.dfu
+
+:: Move Output Files to an Output Folder
+mkdir Output
+move internal.bin Output\
+move external.bin Output\
+
+:: Opening WebDFU
+cls
+start https://ti-planet.github.io/webdfu_numworks/n0110/
